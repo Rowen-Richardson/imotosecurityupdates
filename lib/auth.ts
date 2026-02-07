58039ae1ea187ef30f09bc14affbc095cc4f83ca
Rowen Richardson <richardson.rowen@gmail.com>
2026-02-07 20:35:03 +0200

    Updated emailRedirectTo for signUp function in lib/auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,2 +1,5 @@
-// Assuming this is line 56 within the context of the signUp function
-emailRedirectTo: `${typeof window !== 'undefined' ? window.location.origin : ''}/auth/callback?type=signup`
+// ... (previous lines of code)
+
+    emailRedirectTo: `${typeof window !== 'undefined' ? window.location.origin : ''}/auth/callback?type=signup`,
+
+// ... (following lines of code)
